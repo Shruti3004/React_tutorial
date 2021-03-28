baa01e0927cd32a7be4126763ec21394106d4db0
Shruti3004 <mail2shruti.ag@gmail.com>
2021-03-28 13:41:42 +0530

    PUshing the complete folder at once

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import store from './redux/store';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <CakeContainer/>            
+            </div>
+        </Provider>
+    )
+}
+
+export default App
